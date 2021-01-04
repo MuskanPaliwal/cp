a3b25199d1b04147f755e6c375282df916e6fb33
Muskan Paliwal <muskan10112002@gmail.com>
2021-01-04 22:18:06 +0530

    segregate debug code

---
+++ template/debug.h
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+#include<stdio.h>
+#include<iostream>
+
+#define dbug(...) { cerr<<"[ "; __debug__(#__VA_ARGS__, __VA_ARGS__);}
+#undef endl
+template <typename Arg1,typename Arg2>
+ostream& operator << (ostream& out, const pair<Arg1,Arg2> &x) {
+    return out<<"("<<x.F<<","<<x.S<<")";
+}
+
+template <typename Arg1>
+ostream& operator << (ostream& out, const vector<Arg1> &a) {
+    out<<"[";for(const auto &x:a)out<<x<<",";return out<<"]";
+}
+
+template <typename Arg1>
+ostream& operator << (ostream& out, const set<Arg1> &a) {
+    out<<"[";for(const auto &x:a)out<<x<<",";return out<<"]";
+}
+
+template <typename Arg1,typename Arg2>
+ostream& operator << (ostream& out, const map<Arg1,Arg2> &a) {
+    out<<"[";for(const auto &x:a)out<<x<<",";return out<<"]";
+}
+
+template <typename Arg1>
+void __debug__(const string name, Arg1&& arg1){
+	cerr << name << " : " << arg1 << " ] " << endl;
+}
+
+template <typename Arg1, typename... Args>
+void __debug__(const string names, Arg1&& arg1, Args&&... args){
+	const string name = names.substr(0,names.find(','));
+	cerr<<name<<" : "<<arg1<<" | ";
+	__debug__(names.substr(1+(int)name.size()), args...);
+}
+
+clock_t time_p=clock();
+void oktime(){
+	time_p = clock() - time_p;
+	cerr<<"Time Taken: "<<(float)(time_p)/CLOCKS_PER_SEC<<endl;
+}
